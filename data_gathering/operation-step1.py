5aa89b793b69e6a004b88c11a2f312d78646d1bb
CHEN <NUO.CHEN@shell.com>
2018-07-31 16:01:53 +0100

    rename operation codes files

---
+++ data_gathering/operation-step1.py
@@ -0,0 +1,39 @@
+from utils.import_packages import *
+import utils.hardcode_parameters as param
+from features.make_feature import make_feat_pipeline, make_date_feature, make_lag_feat
+import pickle
+
+
+if __name__ == '__main__':
+    target_date_start = datetime(2018, 8,1)
+
+    model = pickle.load(open("../models/DA_OTC_XGB.pickle.dat", "rb"))
+    df = pd.read_excel(param.data_folder_path + '/trade/OTC_lag_DA.xlsx', sheet_name='Sheet1')
+    df = df.drop(['TRADEDATE'], axis = 1)
+
+    target = 'DA'
+    forecast_v ='VWAP,DA'
+    dateCol = 'DeliveryDate'
+    lag_dict = {target: [96],
+                'VWAP': [96, 96 * 2]}
+    max_lag_days = 2
+    predict_day_start = target_date_start - timedelta(days=max_lag_days)
+    df = df[df[dateCol] >= predict_day_start]
+    X,_ = make_feat_pipeline(target,forecast_v,dateCol,lag_dict,df,standardize=False,dropDate=False)
+    X = X[X[dateCol]>=target_date_start]
+    df = df[df[dateCol]>=target_date_start]
+    feat = X.drop([dateCol], axis =1)
+    prediction = model.predict(feat)
+
+    df['predict_DA'] = prediction
+    df['predict_DA_daily'] = [prediction.mean()]*len(X)
+
+    df = df[['VWAP','DeliveryDate','start','predict_DA','predict_DA_daily']]
+
+    con = Configuration()
+    df['DeliveryDate'] = con.add_time_to_date(df,'DeliveryDate','start')
+    df = df.drop(['start'], axis=1)
+    # merge to operation file
+    df.to_excel(param.data_folder_path + '/operation_pred_DA.xlsx',index=False)
+
+
---
+++ models/operation-step2.py
@@ -0,0 +1,18 @@
+from utils.import_packages import *
+import utils.hardcode_parameters as param
+
+
+if __name__ == '__main__':
+    df = pd.read_excel(param.data_folder_path + '/b_simulation.xlsx', index = False)
+
+    new_df = df[df['Date']>=datetime(2018,8,1)]
+    pred_da = pd.read_excel(param.data_folder_path + '/operation_pred_DA.xlsx', sheet_name='Sheet1')
+
+    new_df = new_df.drop(['predict_DA','predict_DA_daily'],axis=1)
+    new_df = new_df.merge(pred_da.drop(['VWAP'],axis =1), on='DeliveryDate',how= 'inner')
+
+    # df = pd.concat([new_df, df], axis = 0)
+    df = new_df
+    df = df.sort_values(by=['DeliveryDate'])
+    df = df.dropna(subset=['predict_DA','First_Forecast_Volume'])
+    df.to_excel(param.data_folder_path + '/b_simulation_.xlsx', index = False)
