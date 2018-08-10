2a5a61824389a1b07df115ed68c2ae5b332a4e0c
NUO.CHEN <alicechennuo@qq.com>
2018-08-10 09:12:05 +0100

    extend DA-spread

--- models/DA_spread.py
+++ models/DA_spread.py
@@ -4,6 +4,8 @@ from data_gathering.DataChecker import *
 import pickle
 
 if __name__ == '__main__':
+    # target = 'Feed-DA'
+    target = 'Con-Feed'
     df = pd.read_excel(param.operation_folder + '/DA_Spread.xlsx', sheet_name='Sheet1')
 
     for month in range(1, 13):
@@ -14,9 +16,9 @@ if __name__ == '__main__':
         for weekday in range(1, 8):
             for hour in range(0, 24):
                 a =cur_month_df[(cur_month_df['Hour']== hour) & (cur_month_df['WKDY']==weekday)]
-                weekday_hour[weekday][hour] = np.array(a['Feed-DA']).mean()
+                weekday_hour[weekday][hour] = np.array(a[target]).mean()
 
-        savePath = param.operation_folder + '/month_' + str(month) + '.pickle'
+        savePath = param.operation_folder + '/'+ target + '_month_' + str(month) + '.pickle'
         with open( savePath, 'wb') as handle:
             pickle.dump(weekday_hour, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
